a239605b7f3ba9feb8ee22e92a38bec3d905047b
DaSonixier <bundschuja@gmail.com>
2019-02-24 20:55:26 +0100

    Update texts.py

--- texts.py
+++ texts.py
@@ -1,1 +1,19 @@
+def import_texts():
+    return {
 
+    #buttons
+    "refresh_button":
+        "Refresh",
+    "quit_button":
+        "Quit",
+
+    #messages
+    "connected_title":
+        "Connected!",
+    "connected":
+            "You\'re connected to Perfect Privacy.",
+    "disconnected_title":
+        "Disconnected!",
+    "disconnected":
+            "The connection with Perfect Privacy was lost."
+    }
